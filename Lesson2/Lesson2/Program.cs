a39b1c374799eefab241243f48e44904805d5505
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-04-29 21:47:36 +0300

    Homework
    done

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -1,48 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static System.Math;
+
 Console.WriteLine("Lesson2\n");
 
-/*
-int one = 1;
-double two = 2;
-float three = 3f;
-decimal four = 4m;
-
-Console.WriteLine((double)10 / 3);
-
-int five = 5;
-
-Console.WriteLine(++five);
-Console.WriteLine(five);
-
-int six = 6;
-int seven = 7;
-
-Console.WriteLine(seven % six);
-
-int ten = 8;
-int eleven = 9;
-
-Console.WriteLine(eleven > ten);
-
-Console.WriteLine("int: max:" + int.MaxValue + " |min:" + int.MinValue +
-    "\nlong: " + long.MaxValue + " |min:" + long.MinValue +
-    "\nulong: max:" + ulong.MaxValue + " |min:" + ulong.MinValue +
-    "\nshort: max:" + short.MaxValue + " |min:" + short.MinValue);
-
-Math
-double a = 1.2;
-
-Console.WriteLine("Round: " + Math.Round(a) +
-    "\nTruncate: " + Math.Truncate(a) +
-    "\nFloor: " + Math.Floor(a) +
-    "\nCeiling: " + Math.Ceiling(a));
-
-double a = 125;
-
-Console.WriteLine(Math.Cbrt(a));
-
-Console.WriteLine(true || false);
-
-Console.WriteLine(DateTime.Now);
-*/
+Console.WriteLine("Enter the X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Result: " +
+    "\n-6 * x^3 + 5 * x^2 - 10 * x + 15 = " + (-6 * Pow(x, 3) + 5 * Pow(x, 2) - 10 * x + 15) +
+    "\nabs(x) * sin(x) = " + (Abs(x) * Sin(x)) +
+    "\n2 * pi * x = " + (2 * PI * x) +
+    "\nmax(x, y) = " + (Max(x, y)));
 

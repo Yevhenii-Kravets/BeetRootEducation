e4df819b8767a113fe1eedaaf4185b374c6ceacb
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-04-29 11:45:36 +0300

    Lesson2 Create
    create

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lesson2\n");
+
+//int one = 1;
+//double two = 2;
+//float three = 3f;
+//decimal four = 4m;
+
+//Console.WriteLine((double)10 / 3);
+
+//int five = 5;
+
+//Console.WriteLine(++five);
+//Console.WriteLine(five);
+
+//int six = 6;
+//int seven = 7;
+
+//Console.WriteLine(seven % six);
+
+//int ten = 8;
+//int eleven = 9;
+
+//Console.WriteLine(eleven > ten);
+
+//Console.WriteLine("int: max:" + int.MaxValue + " |min:" + int.MinValue +
+//    "\nlong: " + long.MaxValue + " |min:" + long.MinValue +
+//    "\nulong: max:" + ulong.MaxValue + " |min:" + ulong.MinValue +
+//    "\nshort: max:" + short.MaxValue + " |min:" + short.MinValue);
+
+// Math
+//double a = 1.2;
+
+//Console.WriteLine("Round: " + Math.Round(a) +
+//    "\nTruncate: " + Math.Truncate(a) + 
+//    "\nFloor: " + Math.Floor(a) +
+//    "\nCeiling: " + Math.Ceiling(a));
+
+//double a = 125;
+
+//Console.WriteLine(Math.Cbrt(a));
+
+//Console.WriteLine(true || false);
+
+Console.WriteLine(DateTime.Now);

04102f6de480c4a97bf4ee1858e27f4a6aa70b8e
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-05-03 18:24:56 +0300

    Homework
    done

--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -1,52 +1,20 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Lesson3\n");
 
-        /*
-        int number = 0;
-        int sum = 0;
-
-        while (number < 10)
-        {
-            if (number++ % 2 != 0)
-                continue;
-            sum += number;
-        }
-        Console.WriteLine("Sum: " + sum);
-        
-        while (number < 7)
-        {
-            Console.WriteLine("Number: " + number++);
-            if (number == 7)
-            {
-                break;
-            }
-        }
-        */
-        /*
-        do
-        {
-            Console.WriteLine("Number: " + number++);
-            if (number == 7)
-                break;
-        }
-        while (true);
-        */
-        /*
-        string nameFromConsole = Console.ReadLine();
-        int number = 0;
-        bool conversionResult = int.TryParse(nameFromConsole, out number);
-
-        int numberFromConvertClass = Convert.ToInt32(nameFromConsole);
-
-        Console.WriteLine(number);
-
-        */
+        Console.Write("Enter an integer X: ");
+        int X = int.Parse(Console.ReadLine());
+        Console.Write("Enter an integer Y: ");
+        int Y = int.Parse(Console.ReadLine());
 
 
-
-
+        int result = 0;
+        for (int i = (X > Y ? Y : X); i != (X < Y ? Y : X) + 1;)
+            result += i++;
+        Console.WriteLine(result);
     }
 }

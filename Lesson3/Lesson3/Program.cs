4dccf8945c20272e281f76428e4ef18c55744b25
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-05-03 17:03:16 +0300

    Create Lesson3
    lesson

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lesson3\n");
+
+        /*
+        int number = 0;
+        int sum = 0;
+
+        while (number < 10)
+        {
+            if (number++ % 2 != 0)
+                continue;
+            sum += number;
+        }
+        Console.WriteLine("Sum: " + sum);
+        
+        while (number < 7)
+        {
+            Console.WriteLine("Number: " + number++);
+            if (number == 7)
+            {
+                break;
+            }
+        }
+        */
+        /*
+        do
+        {
+            Console.WriteLine("Number: " + number++);
+            if (number == 7)
+                break;
+        }
+        while (true);
+        */
+        /*
+        string nameFromConsole = Console.ReadLine();
+        int number = 0;
+        bool conversionResult = int.TryParse(nameFromConsole, out number);
+
+        int numberFromConvertClass = Convert.ToInt32(nameFromConsole);
+
+        Console.WriteLine(number);
+
+        */
+
+
+
+
+    }
+}

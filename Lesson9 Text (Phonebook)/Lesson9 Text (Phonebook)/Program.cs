918ee3613401432bca48c77e7f01449d6d85f086
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-05-28 02:01:59 +0300

    Update
    add binary search

--- Lesson9 Text (Phonebook)/Lesson9 Text (Phonebook)/Program.cs
+++ Lesson9 Text (Phonebook)/Lesson9 Text (Phonebook)/Program.cs
@@ -8,15 +8,29 @@ internal class Program
           "+--------------------+" // first 20
         + "--------------------+" // last 20
         + "--------------------+"; // num 20
+    enum Attribute
+    {
+        Alphabet,
+        LastName,
+        Number
+    }
 
     static List<Contact> Phonebook = new List<Contact>();
-    static List<Contact> SortPhonebook(List<Contact> phonebook)
+    static List<Contact> SortPhonebook(List<Contact> phonebook, Attribute sortType = Attribute.Alphabet)
     {
-        phonebook = phonebook.OrderBy(contact => contact.FirstName)
-                             .ThenBy(contact => contact.LastName)
-                             .ThenBy(contact => contact.Number)
-                             .ToList();
-        return phonebook;
+        switch (sortType)
+        {
+            case Attribute.Alphabet:
+                return phonebook.OrderBy(contact => contact.FirstName)
+                                     .ThenBy(contact => contact.LastName)
+                                     .ThenBy(contact => contact.Number)
+                                     .ToList();
+            case Attribute.LastName:
+                return phonebook.OrderBy(contact => contact.LastName).ToList();
+            case Attribute.Number:
+                return phonebook.OrderBy(contact => contact.Number).ToList();
+            default: return phonebook;
+        }
     }
 
     static void WritePhonebook()
@@ -39,7 +53,7 @@ internal class Program
         }
     }
 
-    static string ValidationNumber(string? str)
+    static string ValidationNumber(string? str, string num = "")
     {
         string[] operators = { "039", "050", "063", "066", "067", "068", "091", "092", "093", "094",
                                "095", "096", "097", "098", "099", "031", "032", "033", "034", "035",
@@ -84,9 +98,12 @@ internal class Program
                 {
                     bool isFound = false;
                     foreach (var contact in Phonebook)
+                    {
+                        if (contact.Number == num)
+                            continue;
                         if (contact.Number == result)
                             isFound = true;
-
+                    }
                     if (isFound)
                         throw new ArgumentException("This number already exists");
                     else
@@ -108,42 +125,84 @@ internal class Program
 
     static void ShowPhonebook(List<Contact> phonebook)
     {
-        Console.WriteLine(
-            $"\n {Line}\n"
+        Console.WriteLine($"\n {Line}\n"
             + $" |{"First name",20}|"
             + $"{"Last name",20}|"
             + $"{"Number",20}|"
             + $"\n {Line}");
 
-        foreach (var contact in SortPhonebook(phonebook))
-        {
-            Console.WriteLine($" |{contact.FirstName,20}|"
-                  + $"{contact.LastName,20}|"
-                  + $"{contact.Number,20}|"
-                  + $"\n {Line}");
-        }
+        if (phonebook.Count >= 1)
+            foreach (var contact in phonebook)
+                Console.WriteLine($" |{contact.FirstName,20}|"
+                      + $"{contact.LastName,20}|"
+                      + $"{contact.Number,20}|"
+                      + $"\n {Line}");
+        else
+            Console.WriteLine("No found contact");
     }
     static List<Contact> SearchContact()
     {
-        var book = new List<Contact>();
+        List<Contact> phonebook = new List<Contact>();
 
-        Console.Write("\nSearch: ");
-        try
+        Attribute typeSearch = Attribute.Alphabet;
+        bool pass = true;
+        while (pass)
         {
-            var str = ValidationString(Console.ReadLine(), MaximumLength);
+            Console.Clear();
+            Console.WriteLine("Select a search option: " +
+                "\n3. First name" +
+                "\n2. Last name" +
+                "\n1. Number" +
+                "\n0. Back\n");
 
-            foreach (var contact in Phonebook)
-                if ((contact.FirstName).ToLower().Contains(str.ToLower()) ||
-                    (contact.LastName).ToLower().Contains(str.ToLower()) ||
-                    (contact.Number).ToLower().Contains(str.ToLower()))
-                    book.Add(contact);
+            var keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D0 or ConsoleKey.Enter or ConsoleKey.Spacebar:
+                    return null;
+                case ConsoleKey.D1:
+                    phonebook = SortPhonebook(Phonebook, Attribute.Number);
+                    typeSearch = Attribute.Number;
+                    pass = false;
+                    break;
+                case ConsoleKey.D2:
+                    phonebook = SortPhonebook(Phonebook, Attribute.LastName);
+                    typeSearch = Attribute.LastName;
+                    pass = false;
+                    break;
+                case ConsoleKey.D3:
+                    phonebook = SortPhonebook(Phonebook, Attribute.Alphabet);
+                    typeSearch = Attribute.Alphabet;
+                    pass = false;
+                    break;
+                default: break;
+            }
         }
-        catch (Exception e)
+
+        Console.WriteLine("\nEnter the value: ");
+        var searching = ValidationString(Console.ReadLine(), MaximumLength);
+
+        int left = 0;
+        int right = phonebook.Count - 1;
+        while (left <= right)
         {
-            Console.Write(e.Message);
-        }
+            int mid = (left + right) / 2;
+            int result = 0;
+            if (typeSearch == Attribute.Alphabet)
+                result = string.Compare(phonebook[mid].FirstName, searching);
+            else if (typeSearch == Attribute.LastName)
+                result = string.Compare(phonebook[mid].LastName, searching);
+            else
+                result = string.Compare(phonebook[mid].Number, searching);
 
-        return book;
+            if (result < 0)
+                left = mid + 1;
+            else if (result > 0)
+                right = mid - 1;
+            else
+                return new List<Contact>() { phonebook[mid] };
+        }
+        throw new Exception("No found contact");
     }
 
     static void AddContact()
@@ -170,72 +229,79 @@ internal class Program
         Phonebook.Add(contact);
         WritePhonebook();
     }
-    static void DeleteContact(List<Contact> book)
+    static void DeleteContact()
     {
-        if (book.Count == 0)
-            Console.WriteLine("No contact found");
-        else if (book.Count > 1)
-            Console.WriteLine("Specify contact details");
-        else
+        List<Contact> phonebook = new List<Contact>();
+        try
         {
-            foreach (var contact in Phonebook)
-            {
-                if (contact == book[0])
-                {
-                    ShowPhonebook(book);
-
-                    while (true) 
-                    {
-                        Console.WriteLine("Delete this contact?\n0. No\n1. Yes");
-                        switch (Console.ReadKey().Key)
-                        {
-                            case ConsoleKey.D0:
-                                return; 
-                            case ConsoleKey.D1:
-                                Phonebook.Remove(contact);
-                                WritePhonebook();
-                                return;
-                            default: break;
-                        }
-                    }
-                }
-            }
+            phonebook = SearchContact();
         }
-    }
-    static void EditContact(List<Contact> book)
-    {
-        if (book.Count == 0)
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        if (phonebook.Count == 0)
             Console.WriteLine("No contact found");
-        else if (book.Count > 1)
+        else if (phonebook.Count > 1)
             Console.WriteLine("Specify contact details");
         else
         {
-            foreach (var contact in Phonebook)
+            while (true)
             {
-                if (contact == book[0])
+                Console.Clear();
+                ShowPhonebook(phonebook);
+
+                Console.WriteLine("Delete this contact?\n0. No\n1. Yes");
+                var keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
                 {
-                    try
-                    {
-                        ShowPhonebook(book);
-                        Console.Write("\nEnter first name: ");
-                        contact.FirstName = ValidationString(Console.ReadLine(), MaximumLength);
-
-                        Console.Write("\nEnter last name: ");
-                        contact.LastName = ValidationString(Console.ReadLine(), MaximumLength);
-
-                        Console.Write("\nEnter number: ");
-                        contact.Number = ValidationNumber(Console.ReadLine());
-
+                    case ConsoleKey.D0:
+                        return;
+                    case ConsoleKey.D1:
+                        Phonebook.Remove(phonebook[0]);
                         WritePhonebook();
                         return;
-                    }
-                    catch (Exception e) 
-                    { 
-                        Console.WriteLine(e.Message);
-                    }
+                    default: break;
                 }
             }
+
         }
+    }
+    static void EditContact()
+    {
+        List<Contact> phonebook = new List<Contact>();
+        try
+        {
+            phonebook = SearchContact();
+
+
+            if (phonebook.Count == 0)
+                Console.WriteLine("No contact found");
+            else if (phonebook.Count > 1)
+                Console.WriteLine("Specify contact details");
+            else
+            {
+                ShowPhonebook(phonebook);
+                Console.Write("\nEnter first name: ");
+                phonebook[0].FirstName = ValidationString(Console.ReadLine(), MaximumLength);
+
+                Console.Write("\nEnter last name: ");
+                phonebook[0].LastName = ValidationString(Console.ReadLine(), MaximumLength);
+
+                Console.Write("\nEnter number: ");
+                phonebook[0].Number = ValidationNumber(Console.ReadLine(), phonebook[0].Number);
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        WritePhonebook();
+        return;
     }
 
     static void Menu()
@@ -243,12 +309,13 @@ internal class Program
         Console.Clear();
 
         if (Phonebook.Count != 0)
-            Console.WriteLine("5. Search contact"
-                + "\n4. Show all contacts"
-                + "\n3. Edit contact"
-                + "\n2. Delete contact");
+            Console.WriteLine("5. Delete contact"
+                + "\n4. Edit contact"
+                + "\n3. Search contact"
+                + "\n2. Show all contacts");
         Console.WriteLine("1. Add contact"
             + "\n0. Сlose phonebook\n");
+
     }
 
     private static void Main(string[] args)
@@ -259,35 +326,42 @@ internal class Program
         {
             Menu();
             var keyInfo = Console.ReadKey();
-            switch (keyInfo.Key)
+            try
             {
-                case ConsoleKey.D0:
-                    Environment.Exit(0);
-                    break;
-                case ConsoleKey.D5:
-                    ShowPhonebook(SearchContact());
-                    Console.ReadLine();
-                    break;
-                case ConsoleKey.D4:
-                    ShowPhonebook(Phonebook);
-                    Console.ReadLine();
-                    break;
-                case ConsoleKey.D3:
-                    EditContact(SearchContact());
-                    Console.ReadLine();
-                    break;
-                case ConsoleKey.D2:
-                    DeleteContact(SearchContact());
-                    Console.ReadLine();
-                    break;
-                case ConsoleKey.D1:
-                    AddContact();
-                    break;
-                default: break;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.D0:
+                        Environment.Exit(0);
+                        break;
+                    case ConsoleKey.D5:
+                        DeleteContact();
+                        break;
+                    case ConsoleKey.D4:
+                        EditContact();
+                        break;
+                    case ConsoleKey.D3:
+                        ShowPhonebook(SearchContact());
+                        Console.ReadLine();
+                        break;
+                    case ConsoleKey.D2:
+                        ShowPhonebook(SortPhonebook(Phonebook));
+                        Console.ReadLine();
+                        break;
+                    case ConsoleKey.D1:
+                        AddContact();
+                        Console.ReadLine();
+                        break;
+                    default: break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
 }
+
 
 class Contact
 {

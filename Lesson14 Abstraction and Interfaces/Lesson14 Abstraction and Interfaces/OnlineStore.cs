92087adb5678795f06236268f66bc9a43447e1e4
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-06-09 20:41:43 +0300

    Lesson14 Abstraction and Interfaces
    + UML diagram, Person is struct

---
+++ Lesson14 Abstraction and Interfaces/Lesson14 Abstraction and Interfaces/OnlineStore.cs
@@ -0,0 +1,73 @@
+﻿public class OnlineStore : ITrade
+{
+    private string Name { get; set; }
+    private decimal Money { get; set; }
+    private int Tax { get; set; }
+
+    private HashSet<Receipt> Story { get; set; } = new HashSet<Receipt>();
+    public HashSet<Item> Items { get; set; } = new HashSet<Item>();
+    public HashSet<Person> Persons { get; set; } = new HashSet<Person>();
+
+
+    public OnlineStore(string Name, decimal Money, int Tax)
+    {
+        this.Name = Name;
+        this.Money = Money;
+        this.Tax = Tax;
+    }
+
+    public decimal GetMoney() => Money;
+    public void Pay(decimal money) => Money -= money;
+    public void AddMoney(decimal money) => Money += money;
+ 
+    public string Purchase(string personName, string itemName, int quantity)
+    {
+        var item = Items.FirstOrDefault(x => x.ToString() == itemName);
+        if (item == null)
+            return "Item not found";
+        if(item.GetQuantity() < quantity)
+            return "Not enough product";
+
+        var person = Persons.FirstOrDefault(x => x.ToString() == personName);
+        if (person.Equals(default(Person))) // перевіряє значення за замовчуванням, якщо true то змінна пуста
+            return "Person not found";
+
+        var price = item.GetPrice() * (quantity + (quantity / 100 * Tax));
+        if (person.GetMoney() > item.GetPrice() * quantity)
+        {
+            var newPerson = new Person(person.ToString(), person.GetMoney() - price);
+            var newItem = new Item(item.ToString(), item.GetPrice(), item.GetQuantity() - quantity);
+
+            Story.Add(new Receipt(person, item, price, quantity));
+
+            Items.Remove(item); Items.Add(newItem);
+            Persons.Remove(person); Persons.Add(newPerson);
+
+            Money += price;
+        }
+        else
+            return $"{personName} does not have enough money.";
+
+        return $"{personName}, thank you for purchasing {itemName}";
+    }
+    public string PurchaseProductForStore(string itemName, int quantity)
+    {
+        var item = Items.FirstOrDefault(x => x.ToString() == itemName);
+        if (item == null)
+            return "Item not found";
+
+        var price = item.GetPrice() * quantity;
+        if (Money < price)
+            return "Not enough money in the store";
+        else
+        {
+            var newItem = new Item(item.ToString(), item.GetPrice(), item.GetQuantity() + quantity);
+            Items.Remove(item); Items.Add(newItem);
+
+            Money -= price;
+            return $"Bought {quantity} units of {item.ToString()}";
+        }
+    }
+    public string[] GetStory() => Story.Select(receipt => receipt.ToString()).ToArray();
+    public override string ToString() => Name;
+}
---
+++ Lesson14 Abstraction and Interfaces/Lesson14 Abstraction and Interfaces/Program.cs
@@ -0,0 +1,144 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region declared and filled
+
+        Random random = new Random();
+
+        OnlineStore store = new OnlineStore("MyStore", 10000m, 20);
+
+        HashSet<Item> items = new HashSet<Item>();
+        HashSet<Person> persons = new HashSet<Person>();
+
+        string[] products = { "T-shirt", "Jeans", "Sneakers", "Backpack", "Watch", "Headphones", "Smartphone", "Book", "Handbag", "Tablet" };
+        string[] names = { "John", "Emily", "Michael", "Sophia", "William", "Olivia", "James", "Ava", "Benjamin", "Emma" };
+
+        int i = 10;
+        while (i > 0)
+        {
+
+            Item item = new Item(
+                products[random.Next(products.Length)] + random.Next(1000), // зменшив шанс дублю додавши цифру (HashSet) 
+                random.Next(100, 301),
+                random.Next(100));
+            items.Add(item);
+
+            Person person = new Person(
+                names[random.Next(names.Length)] + random.Next(1000),
+                random.Next(500, 2001));
+            persons.Add(person);
+
+            i--;
+        }
+
+        store.Items = items;
+        store.Persons = persons;
+
+        #endregion
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"{store.ToString()}"
+                            + $"\nMoney: {store.GetMoney()}");
+            Console.WriteLine();
+            Console.WriteLine("\n0. Exit"
+                            + "\n1. Add shopper"
+                            + "\n2. Add item"
+                            + "\n3. Purchase a product"
+                            + "\n4. Show history"
+                            + "\n\n");
+
+            var keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D0: // Exit
+                    Environment.Exit(0);
+                    break;
+
+                case ConsoleKey.D1: // Add shopper
+                    Console.WriteLine("\nEnter the name shopper: ");
+                    var namePerson = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(namePerson))
+                    {
+                        Console.WriteLine("Name shopper is empty");
+                        break;
+                    }
+                    Console.WriteLine("Enter the amount of money the buyer has: ");
+                    var moneyIsTrue = int.TryParse(Console.ReadLine(), out var money);
+                    if (moneyIsTrue)
+                        store.Persons.Add(new Person(namePerson, money));
+                    else
+                        Console.WriteLine("Invalid value");
+
+                    break;
+
+                case ConsoleKey.D2: // Add item
+                    Console.WriteLine("\nEnter the name item: ");
+                    var nameItem = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(nameItem))
+                    {
+                        Console.WriteLine("Name item is empty");
+                        break;
+                    }
+
+                    Console.WriteLine("Enter the price item: ");
+                    var priceIsTrue = decimal.TryParse(Console.ReadLine(), out var price);
+                    if (priceIsTrue)
+                        store.Items.Add(new Item(nameItem, price, 0));
+                    else
+                        Console.WriteLine("Invalid price item");
+
+                    break;
+
+                case ConsoleKey.D3: // Purchase a product
+                    foreach (var item in store.Items)
+                        Console.WriteLine(item.ToString());
+
+                    Console.WriteLine("\nEnter the name item: ");
+                    var searchItemName = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(searchItemName))
+                    {
+                        Console.WriteLine("Name item is empty");
+                        break;
+                    }
+
+                    Console.WriteLine("Enter the number of items: ");
+                    var quantityIsTrue = int.TryParse(Console.ReadLine(), out var quantity);
+                    if (quantityIsTrue)
+                    {
+                        var resultPurchase = store.PurchaseProductForStore(searchItemName, quantity);
+                        Console.WriteLine(resultPurchase);
+                    }
+                    else
+                        Console.WriteLine("Invalid quantity item");
+
+                    break;
+
+                case ConsoleKey.D4: // Show history
+                    Console.WriteLine();
+                    foreach (var receipt in store.GetStory())
+                        Console.WriteLine("\n" + receipt);
+
+                    Console.ReadLine();
+                    continue;
+
+                default: break;
+            }
+            
+            // shopping simulation
+            // використав for тому що foreach свариться на зміну колекцій
+            for (int j = 0; j < store.Persons.Count; j++)
+            {
+                var purchase = store.Purchase(
+                    store.Persons.OrderBy(x => Guid.NewGuid()).FirstOrDefault().ToString(),
+                    store.Items.OrderBy(x => Guid.NewGuid()).FirstOrDefault().ToString(),
+                    random.Next(1, 5));
+                Console.WriteLine($"\n{purchase}\nMoney: {store.GetMoney()}$");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}

f5ee6c60f624b7689636cc773c17a9ba1aa07156
Yevhenii-Kravets <ponomarenko.evgenii.k@gmail.com>
2023-06-29 15:29:45 +0300

    Create
    done

---
+++ Lesson21 Reflection/Lesson21 Reflection/TreeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_Reflection
+{
+    public class TreeView
+    {
+        public List<Namespace> Namespaces { get; set; }
+    }
+
+    public class Namespace
+    {
+        public string Name { get; set; }
+    }
+}
